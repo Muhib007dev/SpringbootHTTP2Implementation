d030ee96f7c68cae3a2b0cfd99b112f44aff2723
Muhib Shaikh <muhib.shaikh313@gmail.com>
2020-12-05 21:31:12 +0530

    0.1.1

--- src/main/java/com/example/demo/TestController.java
+++ src/main/java/com/example/demo/TestController.java
@@ -1,14 +1,25 @@
 package com.example.demo;
 
-import org.springframework.web.bind.annotation.GetMapping;
+import java.util.ArrayList;
+
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
 
-	@GetMapping("/testing")
-	public static String test(@RequestParam(value = "fullName", defaultValue = "Hello World") String fullName) {
-		return fullName;
+	String name = "hello";
+
+	@RequestMapping("/testing")
+	public TestModel test(@RequestParam(value = "fullName", defaultValue = "Hello World") String fullName) {
+		TestModel md = new TestModel();
+
+		String name[] = fullName.split(" ");
+
+		md.setFirstName(name[0]);
+		md.setLastName(name[1]);
+
+		return md;
 	}
 }
